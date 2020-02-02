54cd23a0cc24c1f98f999d91b6cc47215628460d
zigzga <447587073@qq.com>
2020-02-02 22:44:27 +0800

    add news page

--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,15 @@
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+// module.exports = {
+//     // had enabled by egg
+//     // static: {
+//     //     enable: true
+//     // },
+//     // package: 'egg-view-nunjucks'
+// };
+
+exports.nunjucks = {
+    enable: true,
+    package: 'egg-view-nunjucks'
 };
